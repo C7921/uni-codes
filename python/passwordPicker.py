5e467300ac7a4efc8d672dfbd660f709de947b16
Connor Sanders <c.ellemsanders@gmail.com>
2020-06-02 20:02:14 +1000

    Updated Password Picker with explanation comments

--- python/passwordPicker.py
+++ python/passwordPicker.py
@@ -13,16 +13,22 @@ nouns = ['apple', 'dinosaur', 'ball', 'goat', 'dragon', 'duck', 'panda', 'hammer
 
 print("Welcome to Password Picker")
 
+# Creates infinite loop
 while True:
+	# Sets range for only 3 passwords
 	for num in range(3):
+		# Randomly gets item from adjectives
 		adjectives = random.choice(adjectives)
+		# Randomly gets item from nouns
 		noun = random.choice(nouns)
+		# Randomly get number between 0 and 100
 		number = random.randrange(0, 100)
+		# Get random special character 
 		special_char = random.choice(string.punctuation)
 
 		password = adjectives + noun + str(number) + special_char
 		print("Your new password is: %s " % password)
-
+		
 	response = input("Wouold you like another set of passwords? (Y/N): ")
 	if response == "n":
 		break
